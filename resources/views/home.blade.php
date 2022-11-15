fa190e0f0f21983111662de0e95276636bf4ce12
yahnodewit <92160439+yahnodewit@users.noreply.github.com>
2022-11-15 14:10:04 +0100

    change class of h1

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 <header>
     <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500;600;700&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500;600;700&family=Satisfy&display=swap" rel="stylesheet">
 
         <title>Coffeecode brew</title>
 
@@ -14,7 +14,7 @@
     <div class="background">
         <img src= "{{ asset('/img/bg.jpg')}}">
         <div class="background__text-info text-info">
-            <h1 class="h1text">Welcome to Coffeecode brewhouse</h1>
+            <h1 class="h1">Welcome to Coffeecode brewhouse</h1>
             <div class="text-info__button-start-brew button-start-brew">
                 <button>Start brewing</button>
             </div>
