bc29d0116007a7cc1dd21f2437abe124ff7f9f93
yahnodewit <92160439+yahnodewit@users.noreply.github.com>
2022-11-15 12:54:01 +0100

    Home screen change: extra divs

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,17 +3,22 @@
 <header>
     <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500;600;700&display=swap" rel="stylesheet">
 
-        <title>Laravel</title>
+        <title>Coffeecode brew</title>
 
         @vite(['resources/css/app.css', 'resources/js/app.js'])
 
 </header>
 <body>
     <div class="background">
-        <img src= "{{ asset('/img/bg.jpg') }}">
-        <h1 class="h1text">Welcome to Coffeecode brewhouse</h1>
-        <button>Start brewing</button>
+        <img src= "{{ asset('/img/bg.jpg')}}">
+        <div class="background__text-info text-info">
+            <h1 class="h1text">Welcome to Coffeecode brewhouse</h1>
+            <div class="text-info__button-start-brew button-start-brew">
+                <button>Start brewing</button>
+            </div>
+        </div>
     </div>
 
 </body>
